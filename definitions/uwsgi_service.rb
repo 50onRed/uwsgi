0f17be7b21c281842352d3bb0a160de749f1cec6
Ivan Dyedov <idyedov@50onred.com>
2013-01-04 11:52:17 -0500

    fix enable_threads

--- definitions/uwsgi_service.rb
+++ definitions/uwsgi_service.rb
@@ -46,7 +46,7 @@ define :uwsgi_service,
   extra_params += " --die-on-term" if params[:die_on_term]
   extra_params += " --close-on-exec" if params[:close_on_exec]
   extra_params += " --disable-logging" if params[:disable_logging]
-  extra_params += " --enable-threads" if params[:enable-threads]
+  extra_params += " --enable-threads" if params[:enable_threads]
   extra_params += " --vacuum" if params[:vacuum]
   extra_params += " --threads %d" % [params[:threads]] if params[:threads]
   extra_params += " --harakiri %d" % [params[:harakiri]] if params[:harakiri]
