2e847c63ea6e27338051c049e516cda0c61abd51
Ivan Dyedov <idyedov@50onred.com>
2013-02-22 14:50:15 -0500

    bump cookbook version to 0.0.10

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "dev@50onred.com"
 license          "All rights reserved"
 description      "Installs/Configures uWSGI"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.9"
+version          "0.0.10"
 depends "python"
 depends "runit"
