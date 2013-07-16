9ec6a4c730853deecb774d23a731f57cab1afe4a
Ivan Dyedov <idyedov@50onred.com>
2013-07-16 11:55:35 -0400

    bump version

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "dev@50onred.com"
 license          "All rights reserved"
 description      "Installs/Configures uWSGI"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.10"
+version          "0.0.11"
 depends "python"
 depends "runit"
