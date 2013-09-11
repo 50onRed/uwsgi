01b53026d59754ebf884b19035e6d3593fbbd243
Guilhem Lettron <guilhem.lettron@optiflows.com>
2013-09-11 17:28:42 +0200

    Add emperor management with LWRP

---
+++ providers/default.rb
@@ -0,0 +1,23 @@
+include Chef::DSL::IncludeRecipe
+
+action :install do
+  include_recipe "uwsgi::emperor"
+
+  if new_resource.file
+    remote_file "config file for #{new_resource.name}" do
+      path ::File.join(node['uwsgi']['emperor']['conf_dir'], new_resource.name + ::File.extname(new_resource.file))
+      source new_resource.file
+      owner "root"
+      group "root"
+      mode "0644"
+      notifies :reload, "service[uwsgi]"
+    end
+  elsif new_resource.parameters
+    include_recipe "conffile"
+    
+    conffile_ini "config file for #{new_resource.name}" do
+      path ::File.join(node['uwsgi']['emperor']['conf_dir'], "#{new_resource.name}.ini")
+      parameters new_resource.parameters
+      notifies :reload, "service[uwsgi]"
+    end
+end
---
+++ recipes/emperor.rb
@@ -0,0 +1,35 @@
+#
+# Cookbook Name:: uwsgi
+# Recipe:: emperor
+#
+# Copyright 2013, Guilhem Lettron <guilhem@lettron.fr>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+case node['uwsgi']['emperor']['service']
+when "upstart"
+  template "/etc/init/uwsgi.conf" do
+    source "uwsgi_upstart.erb"
+    owner "root"
+    group "root"
+    mode "0644"
+    variables :config_dir => node['uwsgi']['emperor']['conf_dir']
+  end
+
+  service "uwsgi" do
+    provider Chef::Provider::Service::Upstart
+    supports :status => true, :restart => true, :reload => true
+    action [ :enable, :start ]
+  end
+end
