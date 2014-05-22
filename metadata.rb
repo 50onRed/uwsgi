83c19d7570aff04e62fd8e93797bcbde8754e525
Henri Roussez <henri.roussez@transgaming.com>
2014-05-22 13:56:33 -0400

    bump version number

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "dev@50onred.com"
 license          "MIT"
 description      "Installs/Configures uWSGI"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.1"
+version          "0.1.2"
 
 depends          "python"
 depends          "runit"
