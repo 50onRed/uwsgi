f04e182baf24a8bd15c02a3fdbc7ba2fabf81244
Kris Vincent <kvincent@gannett.com>
2015-07-06 13:27:03 -0400

    GDAES-1253 - Updates to integration testing for uwsgi

--- test/integration/gem/serverspec/install_gem_spec.rb
+++
@@ -1,6 +0,0 @@
-require_relative 'spec_helper'
-
-describe command('/opt/chef/embedded/bin/gem list') do
-  its(:stdout) { should contain('uwsgi') }
-  its(:exit_status) { should eq 0 }
-end
--- test/integration/gem/serverspec/spec_helper.rb
+++
@@ -1,8 +0,0 @@
-require 'serverspec'
-require 'pathname'
-require 'json'
-
-set :backend, :exec
-
-set :path, '/sbin:/usr/local/sbin:/usr/local/bin:/usr/sbin:$PATH'
-$node = ::JSON.parse(File.read('/tmp/serverspec/node.json'))
